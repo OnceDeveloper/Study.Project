7dd2616ba8e1182fe25bd2773ca5b05edf753ef1
JungHyewon <vkfks4607@gmail.com>
2021-12-10 11:03:26 +0900

    2021-12-10 todolist 수정 기능 완료

--- todolist/src/components/todo.jsx
+++ todolist/src/components/todo.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Button from './common/Button';
 
@@ -7,9 +7,12 @@ const Li = styled.li`
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  border-bottom: 1px solid #c9c9c5;
   padding: 20px 0 10px;
+  border-bottom: 1px solid #c9c9c5;
 
+  div:nth-child(1) {
+    width: 80%;
+  }
   button {
     margin-right: 10px;
   }
@@ -21,14 +24,17 @@ const Li = styled.li`
     padding: 0;
   }
 `;
-
 const CheckInput = styled.input`
-  width: 15px;
-  height: 15px;
+  width: 20px;
+  height: 20px;
   margin: 0 10px 0 0;
 `;
 const TodoLabel = styled.label`
   display: inline-block;
+  width: 90%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
   text-decoration: ${props => (props.isCheck ? ' line-through' : 'none')};
 `;
 const TodoInput = styled.input`
@@ -37,55 +43,59 @@ const TodoInput = styled.input`
   margin-right: 20px;
   padding: 0 10px;
 `;
-const Todo = ({ id, value, editTodo, delTodo }) => {
-  const [isCheck, setIsCheck] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [editValue, setEditValue] = useState(value);
 
-  const editTodolist = useCallback(
-    e => {
-      if (e.key === 'Enter') {
-        editTodo({ id: id, value: editValue });
-      }
-    },
-    [editTodo, isEdit, editValue, id]
-  );
+const Todo = ({ id, value, editTodo, delTodo }) => {
+  const [isCheck, setIsCheck] = useState(false); //checkbox 체크 여부
+  const [isEdit, setIsEdit] = useState(false); //edit button 클릭 여부
+  const [editValue, setEditValue] = useState(value); //수정한 todo value
+
+  /**
+   * isEdit-false : 수정 후 확인 버튼을 클릭한 경우 || 수정 후 input 값에서 Enter 입력한 경우
+   * value : 현재 todo value 값
+   * editValue : 수정한 todo value 값
+   */
+  useEffect(() => {
+    if (!isEdit && value !== editValue) {
+      editTodo({ id: id, value: editValue });
+    }
+  }, [isEdit]);
 
   return (
     <Li>
-      <ul>
-        <li>
-          {isEdit ? (
-            <TodoInput
-              type="text"
-              value={editValue}
-              onChange={e => setEditValue(e.target.value)}
-              onKeyDown={editTodolist}
-            ></TodoInput>
-          ) : (
-            <>
-              <CheckInput
-                id={id}
-                type="checkbox"
-                onChange={() => setIsCheck(!isCheck)}
-              />
-              <TodoLabel htmlFor={id} isCheck={isCheck}>
-                {value}
-              </TodoLabel>
-            </>
-          )}
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Button
-            type="B"
-            text={isEdit ? '확인' : '수정'}
-            onClick={() => setIsEdit(!isEdit)}
-          ></Button>
-          <Button type="C" text="삭제" onClick={() => delTodo(id)}></Button>
-        </li>
-      </ul>
+      <div>
+        {isEdit ? (
+          <TodoInput
+            type="text"
+            value={editValue}
+            onChange={e => setEditValue(e.target.value)}
+            onKeyDown={e => {
+              if (e.key === 'Enter') {
+                setIsEdit(!isEdit);
+              }
+            }}
+          ></TodoInput>
+        ) : (
+          <>
+            <CheckInput
+              id={id}
+              type="checkbox"
+              onChange={() => setIsCheck(!isCheck)}
+              checked={isCheck}
+            />
+            <TodoLabel htmlFor={id} isCheck={isCheck}>
+              {value}
+            </TodoLabel>
+          </>
+        )}
+      </div>
+      <div>
+        <Button
+          type="B"
+          text={isEdit ? '확인' : '수정'}
+          onClick={() => setIsEdit(!isEdit)}
+        ></Button>
+        <Button type="C" text="삭제" onClick={() => delTodo(id)}></Button>
+      </div>
     </Li>
   );
 };
