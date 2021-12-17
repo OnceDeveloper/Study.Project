34e64e75e226d2293e121a2f6af36d2dbb4aeddd
JungHyewon <vkfks4607@gmail.com>
2021-12-17 13:58:38 +0900

    2021-12-17 수원과학대학교 css 작업중

---
+++ css_study/suwon_science_university/main/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  //   const specBackImg = document.getElementsByClassName('div-spec');
+  //   function ChangeImage(strImage) {
+  //     document.getElementsByClassName('div-spec')
+  //   }
+  //   setTimeout(ChangeImage('../img/2020/main2.png'), 1000);
+};
