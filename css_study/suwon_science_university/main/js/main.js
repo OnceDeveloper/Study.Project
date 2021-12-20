035b65610a0406e051416d4554117e93ad7fc229
JungHyewon <vkfks4607@gmail.com>
2021-12-20 15:49:11 +0900

    2021-12-20 main page css 완료 스타일 관련 js 추가 필요

--- css_study/suwon_science_university/main/js/main.js
+++ css_study/suwon_science_university/main/js/main.js
@@ -1,7 +1,8 @@
 window.onload = function () {
-  //   const specBackImg = document.getElementsByClassName('div-spec');
   //   function ChangeImage(strImage) {
-  //     document.getElementsByClassName('div-spec')
+  //     document.getElementsByClassName('div-spec')[0].style.backgroundImage =
+  //       'url(' + strImage + ')';
   //   }
-  //   setTimeout(ChangeImage('../img/2020/main2.png'), 1000);
+  //   setTimeout(ChangeImage('img/2020/main2.png'), 3000);
+  //   setTimeout(ChangeImage('img/2020/main3.png'), 3000);
 };
