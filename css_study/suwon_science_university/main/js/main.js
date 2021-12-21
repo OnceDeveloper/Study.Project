306ec70a417687211dd0e5f630a0112e5dd6d8aa
JungHyewon <vkfks4607@gmail.com>
2021-12-21 11:18:46 +0900

    2021-12-21 pc version css 및 js 완료, 반응형 작업 필요

--- css_study/suwon_science_university/main/js/main.js
+++ css_study/suwon_science_university/main/js/main.js
@@ -1,8 +1,23 @@
 window.onload = function () {
-  //   function ChangeImage(strImage) {
-  //     document.getElementsByClassName('div-spec')[0].style.backgroundImage =
-  //       'url(' + strImage + ')';
-  //   }
-  //   setTimeout(ChangeImage('img/2020/main2.png'), 3000);
-  //   setTimeout(ChangeImage('img/2020/main3.png'), 3000);
+  // 3초마다배경 이미지 전환
+  let i = 1;
+  setInterval(function () {
+    i++;
+    if (i === 4) i = 1;
+    $('.div-spec').css('background-image', 'url(img/2020/main' + i + '.png)');
+  }, 3000);
+  // active가 없는 엘레먼트가 선택된 경우에만 active class를 추가해주고
+  // 기존의 active class를 가지고 있던 엘레먼트에선 삭제해줌
+  $('.program-category-ul > li > a.category').on('click', function (e) {
+    const target = $(e.target);
+
+    if (!target.hasClass('category-active')) {
+      target.addClass('category-active');
+      target
+        .parent()
+        .siblings()
+        .children('.category-active')
+        .removeClass('category-active');
+    }
+  });
 };
