a9715ac326f9e8ce7666bb1a34ed221f84329b8c
JungHyewon <vkfks4607@gmail.com>
2021-12-30 09:28:40 +0900

    2021-12-30 1. main.js import 전 jQuery import 먼저 실행되도록 코드 위치 변경 2. main.js 로직 수정

--- css_study/suwon_science_university/main/js/main.js
+++ css_study/suwon_science_university/main/js/main.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+$(document).ready(function () {
   // 3초마다배경 이미지 전환
   let i = 1;
   setInterval(function () {
@@ -6,18 +6,17 @@ window.onload = function () {
     if (i === 4) i = 1;
     $('.div-spec').css('background-image', 'url(img/2020/main' + i + '.png)');
   }, 3000);
-  // active가 없는 엘레먼트가 선택된 경우에만 active class를 추가해주고
-  // 기존의 active class를 가지고 있던 엘레먼트에선 삭제해줌
-  $('.program-category-ul > li > a.category').on('click', function (e) {
-    const target = $(e.target);
 
+  // active가 없는 엘레먼트가 선택된 경우에만
+  // active class를 가지고 있던 엘레먼트 모두 삭제해준 후
+  // active class를 추가해줌
+  $('.program-category-ul > li > a.category').on('click', function (e) {
+    const target = $(this);
     if (!target.hasClass('category-active')) {
+      $('.program-category-ul > li > a.category').removeClass(
+        'category-active'
+      );
       target.addClass('category-active');
-      target
-        .parent()
-        .siblings()
-        .children('.category-active')
-        .removeClass('category-active');
     }
   });
-};
+});
